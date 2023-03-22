495bed99167e39836f624335d0df0f8f9c64415e
Nikita <teslay.123@gmail.com>
2023-03-22 18:11:02 +1000

    Мне сказали "погуляй пока класс не написан" :(

--- Fight.cs
+++ Fight.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace Rogulike_way
 {   //Временно создам классы Монстра и Героя, потом переделаю, когда будут у Андрея
-    //
+    /*public class Hero_Nikita
+    {
+        public double health = 100;
+        public int lebel = 1;
+        public int experience = 0;
+        public double boost = 1.22;
+        public double damage = 40;
+        public double stamina = 100;
+    }*/
     public class Fight
     {
         //Начало файта
-        public void Start()
+        public void Start(Hero_Nikita Hero, Hero_Nikita Monsters)
         {
 
         }
