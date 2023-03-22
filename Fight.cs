a3248f85e2e0c1ab609b4bcfb706c24b43abc74f
Nikita <teslay.123@gmail.com>
2023-03-22 17:50:01 +1000

    Добавил немного своего

---
+++ Fight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rogulike_way
+{   //Временно создам классы Монстра и Героя, потом переделаю, когда будут у Андрея
+    //
+    public class Fight
+    {
+        //Начало файта
+        public void Start()
+        {
+
+        }
+    }
+}
