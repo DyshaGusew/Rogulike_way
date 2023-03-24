0e7014c0af089ed255cb3d3edaa70dc7d1b28548
Nikita <teslay.123@gmail.com>
2023-03-24 19:03:13 +1000

    Решили момент с консолью

--- Fight.cs
+++ Fight.cs
@@ -1,26 +1,19 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Rogulike_way
-{   //Временно создам классы Монстра и Героя, потом переделаю, когда будут у Андрея
-    /*public class Hero_Nikita
-    {
-        public double health = 100;
-        public int lebel = 1;
-        public int experience = 0;
-        public double boost = 1.22;
-        public double damage = 40;
-        public double stamina = 100;
-    }
+{   
     public class Fight
     {
         //Начало файта
-        public void Start(Hero_Nikita Hero, Hero_Nikita Monsters)
+        public void Start()
         {
+            Console.Clear();
+
 
         }
-    }*/
-}
+    }
+}*/
