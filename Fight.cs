14547b7007d71d2d464d94e1f24b7a3391b988e7
Nikita <teslay.123@gmail.com>
2023-03-24 19:18:53 +1000

    Добавил классы в Fight

--- Fight.cs
+++ Fight.cs
@@ -1,11 +1,32 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Rogulike_way
-{   
+{/*public class Hero
+    public int[] coordinates;
+    public double StaticHealht = 100;
+    public double NowHealht = 100;
+    public double StaticStamina = 100;
+    public double NowStamina = 100;
+    public double damage = 40;
+    public int level = 1;
+    public int expirience = 0;
+    public double boost = 1.0;
+*/
+
+ /*public class Monsters
+            public string name;   //Я создал для тестирования, можно удалить
+            public int[] coordinates;
+            public double StaticHealht;
+            public double NowHealht;
+            public double damage;
+            public int level;
+            public int expirience; // При смерти моба можно передавать его опыт герою
+            public double boost;
+ */
     public class Fight
     {
         //Начало файта
@@ -16,4 +37,4 @@ namespace Rogulike_way
 
         }
     }
-}*/
+}
